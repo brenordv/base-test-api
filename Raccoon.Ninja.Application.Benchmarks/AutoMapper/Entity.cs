2caeb0c654c77269d97b9f6f70b95067c5c1e061
Breno RdV <noreply@raccoon.ninja>
2023-01-07 02:39:18 -0500

    Added Benchmarks project

---
+++ Raccoon.Ninja.Application.Benchmarks/AutoMapper/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Raccoon.Ninja.Application.Benchmarks.AutoMapper;
+
+public record Entity()
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public float BaseValue { get; init; }
+    public float TaxPercent { get; init; }
+    public DateTime CreatedAt { get; init; }
+}
---
+++ Raccoon.Ninja.Application.Benchmarks/AutoMapper/Model.cs
@@ -0,0 +1,20 @@
+﻿namespace Raccoon.Ninja.Application.Benchmarks.AutoMapper;
+
+public record Model()
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public float FinalValue { get; init; }
+    
+    public static explicit operator Model(Entity entity)
+    {
+        return new Model
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+            Description = entity.Description,
+            FinalValue = entity.BaseValue + entity.BaseValue * entity.TaxPercent
+        };
+    }
+}
