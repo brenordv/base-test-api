7a72380deae2383d132cbf20e4cc942c733256ad
Breno RdV <noreply@raccoon.ninja>
2023-01-21 01:49:39 -0500

    Updated readme + removed benchmark project

--- Raccoon.Ninja.Application.Benchmarks/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-using Raccoon.Ninja.Application.Benchmarks.AutoMapper;
-
-Console.WriteLine("Running AutoMapper Benchmarks!");
-BenchmarkRunner.Run<AutoMapperBenchmarks>();
